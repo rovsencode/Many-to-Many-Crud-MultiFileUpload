d6d08a33c0675073a090d877b15e340943802649
rovsencode <rovshanakh@code.edu.az>
2023-03-05 17:07:48 +0400

    add prodcut create page

---
+++ FirelloProject/Areas/AdminArea/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using FirelloProject.DAL;
+using FirelloProject.Extentions;
+using FirelloProject.Models;
+using FirelloProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirelloProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IWebHostEnvironment _env;
+
+        public ProductController(AppDbContext appDbContext, IWebHostEnvironment env)
+        {
+            _appDbContext = appDbContext;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appDbContext.Products.Include(p => p.ProductImages).Include(p=>p.Category).ToList());
+        }
+        public IActionResult Create()
+        {
+
+            //ViewBag.Categories = new SelectList(_appDbContext.Categories.ToList(), "Id", "Name");
+            ViewBag.Categories = _appDbContext.Categories.ToList();
+          
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(ProductCreateVM productCreateVM)
+        {
+            ViewBag.Categories = _appDbContext.Categories.ToList();
+            if (!ModelState.IsValid) return View();
+            List<ProductImage> productImages = new();
+            foreach (var photo in productCreateVM.Photos)
+            {
+                if (!photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "only image ");
+                    return View();
+                }
+                if (photo.CheckImageSize(500))
+                {
+                    ModelState.AddModelError("Photo", "olcu boyukdur ");
+                    return View();
+                }
+                ProductImage productImage = new();
+                productImage.ImageUrl = photo.SaveImage(_env,"img",photo.FileName);
+                productImages.Add(productImage);
+
+
+            }
+
+            Product newproduct = new();
+            newproduct.Name = productCreateVM.Name;
+            newproduct.Price = productCreateVM.Price;
+            newproduct.CategoryID = productCreateVM.CategoryId;
+            newproduct.ProductImages = productImages;
+            _appDbContext.Products.Add(newproduct);
+            _appDbContext.SaveChanges();
+         
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirelloProject/ViewModels/ProductCreateVM.cs
@@ -0,0 +1,10 @@
+﻿namespace FirelloProject.ViewModels
+{
+    public class ProductCreateVM
+    {
+        public  string ?Name { get; set; }
+        public double Price { get; set; }
+        public int? CategoryId { get; set; }
+        public IFormFile[]? Photos { get; set; }
+    }
+}
